649cb49542d571b72ca5690732a83d8c25b6c25c
Kien Nguyen <ryan@exgo.vn>
2019-10-04 08:59:48 +0700

    -remove caching image +circle tranformation pet avatar

--- app/src/main/java/com/evn/petpet/MainActivity.java
+++ app/src/main/java/com/evn/petpet/MainActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
@@ -15,13 +19,17 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Map<String, String> pets = new HashMap<>();
-
+    ProgressBar imageLoading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initData();
+
+        imageLoading = findViewById(R.id.imageLoading);
+
+        displayPetImage("dog");
 
         findViewById(R.id.go).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     void initData() {
         pets.put("dog", "https://images.squarespace-cdn.com/content/v1/5a7f26bf268b96210912ae2d/1522770168413-11SGHGU3890Z0Q2PW22P/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVH33scGBZjC30S7EYewNF5iKKwhonf2ThqWWOBkLKnojuqYeU1KwPvsAK7Tx5ND4WE/cody_golden.jpg");
         pets.put("cat", "https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg");
-        pets.put("rabit", "https://assets.change.org/photos/4/nh/bb/HfNHbbpayJVOjKD-400x400-noPad.jpg?1530422195");
+        pets.put("rabbit", "https://assets.change.org/photos/4/nh/bb/HfNHbbpayJVOjKD-400x400-noPad.jpg?1530422195");
     }
 
     void displayPetImage(String name) {
@@ -48,9 +56,24 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        imageLoading.setVisibility(View.VISIBLE);
         Picasso.get()
                 .load(petImage)
-                .into(((ImageView) findViewById(R.id.petImage)));
+                .transform(new CircleTransform())
+                .memoryPolicy(MemoryPolicy.NO_CACHE,
+                        MemoryPolicy.NO_STORE)
+                .into(((ImageView) findViewById(R.id.petImage)),
+                        new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                imageLoading.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onError(Exception e) {
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
     }
 
     String getPetImageByName(String name) {
